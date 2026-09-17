1b48f78b6aa63e1fb039562b3962420f83625921
AakarshBot <driven2goal@gmail.com>
2026-09-17 05:34:49 +0530

    Clean Test Phase runtime initialization

--- pages/Test_Phase.py
+++ pages/Test_Phase.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import streamlit as st
 
 import ultimate_bot
+import visual_runtime
 from audio_runtime import patch_audio_pipeline
 from factory_runtime import install_safe_exception_hook, patch_dashboard_runtime
 from provider_runtime import patch_provider_adapters
@@ -25,14 +26,14 @@ def _init_runtime() -> None:
         patch_story_selection(ultimate_bot)
         harden_editorial_defaults(ultimate_bot)
         patch_quality_control(ultimate_bot)
-        install_visual_qa_bridge(ultimate_bot.visual_runtime if hasattr(ultimate_bot, "visual_runtime") else __import__("visual_runtime"))
+        install_visual_qa_bridge(visual_runtime)
         patch_visual_pipeline(ultimate_bot)
         patch_audio_pipeline(ultimate_bot)
         patch_provider_adapters(ultimate_bot)
         ultimate_bot._dashboard_runtime_initialized = True
     else:
         harden_editorial_defaults(ultimate_bot)
-        install_visual_qa_bridge(__import__("visual_runtime"))
+        install_visual_qa_bridge(visual_runtime)
         patch_provider_adapters(ultimate_bot)
     bind_dashboard_patches(ultimate_bot)
 
