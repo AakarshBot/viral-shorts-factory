d2cb3a013f28ff4011dfc17b30e07d81f93c9e71
AakarshBot <driven2goal@gmail.com>
2026-09-18 23:48:18 +0530

    Stop binding legacy visual asset fetcher

--- runtime_bindings.py
+++ runtime_bindings.py
@@ -341,7 +341,7 @@ def bind_dashboard_patches(bot):
     names = (
         "gather_and_filter_stories", "editorial_gate_batch", "process_scored_candidates", "validate_script",
         "self_critique_pass", "write_script", "generate_voiceover_and_timestamps", "process_visuals_async",
-        "fetch_scene_asset", "get_trend_signal_bonus", "auto_pilot_selection", "run_analytics_sweep",
+        "get_trend_signal_bonus", "auto_pilot_selection", "run_analytics_sweep",
         "token_overlap_ratio", "upload_to_youtube", "generate_karaoke_clip",
     )
     bound = []
