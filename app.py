82aa2b3c96b768108d375b88e9c6f77d90e895be
AakarshBot <driven2goal@gmail.com>
2026-09-18 22:17:20 +0530

    Fix stale editorial defaults import

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ from diagnostics_runtime import run_offline_diagnostics
 from factory_runtime import install_safe_exception_hook, patch_dashboard_runtime
 from provider_runtime import patch_provider_adapters
 from quality_runtime import patch_quality_control
-from runtime_bindings import bind_dashboard_patches, harden_editorial_defaults
+from runtime_bindings import bind_dashboard_patches
 from semantic_runtime import patch_semantic_dedup
 from story_ranker import patch_story_selection
 from visual_content_runtime import patch_content_first_visuals as patch_visual_pipeline
@@ -101,7 +101,6 @@ def initialise_runtime() -> None:
         patch_dashboard_runtime(ultimate_bot)
         patch_semantic_dedup()
         patch_story_selection(ultimate_bot)
-        harden_editorial_defaults(ultimate_bot)
         patch_quality_control(ultimate_bot)
         install_visual_qa_bridge(visual_runtime)
         patch_visual_pipeline(ultimate_bot)
@@ -114,7 +113,6 @@ def initialise_runtime() -> None:
         )
         ultimate_bot._dashboard_runtime_initialized = True
     else:
-        harden_editorial_defaults(ultimate_bot)
         install_visual_qa_bridge(visual_runtime)
         patch_provider_adapters(ultimate_bot)
 
